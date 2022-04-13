5e2cb11ce00e54ddb8a16c6be9e178e2337006ac
Daniela Beltrán Cruz <83661689+DanielaBeltranCruz@users.noreply.github.com>
2022-04-13 13:10:43 -0600

    Create main.js

---
+++ weekly_mission_1/example8/main.js
@@ -0,0 +1,10 @@
+// Example 8: EcmaScript Export Default
+// Ejecuta el siguiente comando: node main.js
+// Ejecuta el siguiente comando: node main_2.js
+
+
+// Importando la clase MyLogger
+import MyLogger from './logger.js'
+
+const logger = new MyLogger('info')
+logger.log('Hello World')
